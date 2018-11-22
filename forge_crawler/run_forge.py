93dd0a2ede9e6838299aec94f2af9ff0e318c9b1
Chand Chen <changduchen@gmail.com>
2018-11-22 21:09:50 +0800

    Update crawlers with issue detail user informations

--- forge_crawler/run_forge.py
+++ forge_crawler/run_forge.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import requests
 import json
+import csv
 
 from bs4 import BeautifulSoup
 
@@ -48,16 +49,29 @@ class Authentication:
             print('登陆失败！')
 
 
-def fetch_user_issue_list(state, username):
-    filter_url = 'https://forge.channelfix.com/channelfix/channelfix/issues?\
-        scope=all&utf8=%E2%9C%93&state={}&assignee_username={}'.format(
-        state, username)
+def fetch_user_issue_list(state, assignee, author='', milestone='', label=''):
+    if author:
+        filter_url = "https://forge.channelfix.com/channelfix/channelfix/issues?\
+            cope=all&utf8=%E2%9C%93&state={}&author_username={}&\
+            assignee_username={}&milestone_title={}&label_name[]={}".format(
+            state, author, assignee, milestone, label)
+    else:
+        filter_url = 'https://forge.channelfix.com/channelfix/channelfix/issues?\
+            scope=all&utf8=%E2%9C%93&state={}&assignee_username={}'.format(
+            state, assignee)
 
     html = sessions.get(filter_url, headers=headers).text
     soup = BeautifulSoup(html, features="html.parser")
 
     contents = soup.find(
         'ul', class_="issues-list").findAll('li', class_="issue")
+
+    c_file = open(
+        "forge_crawler/search_results/search_results_[{}].csv".format(
+            assignee), "w")
+    search_file = csv.writer(c_file)
+    search_file.writerow(csv_readers)
+
     for content in contents:
         counts = contents.index(content)
         print('正在获取第1页第{}条数据'.format(counts + 1))
@@ -89,15 +103,29 @@ def fetch_user_issue_list(state, username):
         except Exception as e:
             labels = []
 
-        infos = {
-            'title': title,
-            'link': link,
-            'number': number,
-            'author': author,
-            'labels': labels
-        }
+        if link:
+            detail_url = 'https://forge.channelfix.com' + link
+            html0 = sessions.get(detail_url, headers=headers).text
+            soup0 = BeautifulSoup(html0, features="html.parser")
+            contents0 = soup0.find(
+                'ul', class_="main-notes-list").findAll(
+                'li', class_="timeline-entry")
+            for content0 in contents0:
+                try:
+                    owner = content0.find(
+                        'span', class_="note-header-author-name").get_text()
+                    action = content0.find(
+                        'span', class_="system-note-message").find(
+                        'span').get_text()
+                    action_info = {
+                        'owner': owner,
+                        'action': action
+                    }
+                except Exception as e:
+                    action_info = {}
 
-        print('>>>', infos)
+        search_file.writerow(
+            [number, title, author, link, labels, action_info])
 
 
 if __name__ == '__main__':
@@ -110,7 +138,26 @@ if __name__ == '__main__':
     cs = Authentication(email, password)
     cs.login()
 
-    username = input('请输入要获取的用户名：')
-    state = input('请输入要获取的状态：')
+    keywords = {}
 
-    fetch_user_issue_list(state, username)
+    state = input('状态(opened/closed/all): ')
+    if state:
+        keywords['state'] = state
+    else:
+        print('状态不能为空！')
+    assignee = input('被指派人(username): ')
+    if assignee:
+        keywords['assignee'] = assignee
+    else:
+        print('指派人不能为空！')
+    author = input('创建人(username): ')
+    if author:
+        keywords['author'] = author
+    milestone = input('里程碑(空格用%20)： ')
+    if milestone:
+        keywords['milestone'] = milestone
+    label = input('标签(Bug/空格用%20)： ')
+    if label:
+        keywords['label'] = label
+
+    fetch_user_issue_list(**keywords)
